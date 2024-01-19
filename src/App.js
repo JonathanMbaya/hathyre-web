47fb4b06cd520275a7eeb60953ab453f3cb46b68
NateDev <jonathanmbaya13@gmail.com>
2024-01-19 16:37:41 +0100

    Mise en place React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProductPage from './pages/ProductPage';
+// Importez d'autres pages au besoin.
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/" element={<ProductPage />} />
+        {/* Ajoutez d'autres routes au besoin. */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
