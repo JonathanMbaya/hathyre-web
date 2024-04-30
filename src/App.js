5a114cb231afcf795baf68fd6563c27c3ecb6b02
unknown <jonathanmbaya13@gmail.com>
2024-04-30 07:53:55 +0200

    Nettoyage et requête globale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-// import { useNavigate, useParams } from 'react-router-dom';
-// import axios from 'axios';
 import PublicRouter from '../src/pages/public/PublicRouter.jsx';
 import AdminRouter from '../src/pages/admin/AdminRouter.jsx';
 
@@ -10,7 +8,7 @@ function App() {
    <>
       <PublicRouter />
       <AdminRouter />
-   </> // Fournir les valeurs du contexte aux composants enfants
+   </>
 
 
   );
